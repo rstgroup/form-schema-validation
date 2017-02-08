0a6ffa19634c714cce610bb285b902b0a325c271
Mariusz Przodała <mariusz.przodala@proserwit.pl>
2017-02-08 22:47:39 +0100

    fix output path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: './libs/',
+        path: './lib/',
         filename: "index.js",
         libraryTarget: "umd"
     },
